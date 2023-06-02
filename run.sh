eb7fadd7d4a61e6bad641d2b41ad0e2895b88476
maxhougas <max.hougas@gmail.com>
2023-06-02 00:15:14 -0500

    big error found at run time updates to run.sh for persistance of mods

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -dp 2302-2306:2302-2306/udp -v $(pwd)/save:/root/.local/share/Arma\ 3\ -\ Other\ Profiles/Player/ -v $(pwd)/mods:/root/Steam/steamapps/common/Arma\ 3\ Server/mods --name arma-s arma-i
+docker run -dp 2302-2306:2302-2306/udp -v $(pwd)/107410:$(cat pathmod) -v $(pwd)/mods:/root/Steam/steamapps/common/Arma\ 3\ Server/mods --name arma-s arma-i
