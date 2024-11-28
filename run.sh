66ae889837cfef75b38f9c810cbf0b3fe47caa20
maxhougas <max.hougas@gmail.com>
2024-11-28 12:01:51 -0600

    made ports explicit in run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker run -dp 2302-2306:2302-2306/udp -v $(pwd)/save:/root/.local/share/Arma\ 3\ -\ Other\ Profiles/Player -v $(pwd)/mods:/root/Steam/steamapps/common/Arma\ 3\ Server/mods --name arma arma
+docker run -d \
+ -p 2344-2345:2344-2345/tcp \
+ -p 27015:27015/tcp \
+ -p 27036:27036/tcp \
+ -p 2302-2306:2302-2306/udp \
+ -p 2344:2344/udp \
+ -p 27015:27015/udp \
+ -p 27031-27036:27031-27036/udp \
+ -v $(pwd)/save:/root/.local/share/Arma\ 3\ -\ Other\ Profiles/Player \
+ -v $(pwd)/mods:/root/Steam/steamapps/common/Arma\ 3\ Server/mods \
+ --name arma arma
