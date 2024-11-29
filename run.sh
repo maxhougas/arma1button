0f359f7bff5ab5b92b26a06857050a8693831e45
maxhougas <max.hougas@gmail.com>
2024-11-29 09:00:26 -0600

    removed extraneous \ from run.sh

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ docker run -d \
  -p 27031-27036:27031-27036/udp \
  -v $(pwd)/save:/root/.local/share/Arma\ 3\ -\ Other\ Profiles/Player \
  -v $(pwd)/mods:/root/Steam/steamapps/common/Arma\ 3\ Server/mods \
- --name arma arma \
+ --name arma arma
