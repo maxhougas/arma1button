cf17f3f48455c8565bf72bfb73a468fe733500c8
maxhougas <max.hougas@gmail.com>
2024-12-02 14:35:55 -0600

    troubleshooting

--- finalizemods.sh
+++ finalizemods.sh
@@ -16,8 +16,6 @@ mname=$(echo $line | grep -Po "(?<==)[A-Za-z0-9._-]*")
 done
 
 #Build modline
-modline1=$(grep -Po '(?<==)[a-zA-Z0-9._-]*' modlist | sed 's:^:mods/lns/:g' | sed -z 's:\n:;:g' | sed 's:;$:":')
-$dexec cp mods/dlclist mods/modline
-$dexec sed -zi 's:\n:;:g' mods/modline
-$dexec sed -i 's:^:\":' mods/modline
-$dexec sed -i "s:$:$modline1:" mods/modline
+echo -n $(sed -z 's:\n:;:g' dlclist | sed 's:^:\":') > modline
+echo -n $(grep -Po '(?<==)[a-zA-Z0-9._-]*' modlist | sed 's:^:mods/lns/:g' | sed -z 's:\n:;:g' | sed 's:;$:":') >> modline
+docker cp modline arma:"$arma/mods/modline"
