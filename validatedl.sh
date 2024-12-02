e833bad37dee1a44fc740eb1f93aa8b4cfc39ba8
maxhougas <max.hougas@gmail.com>
2024-12-02 05:54:15 -0600

    fixed validatedl

--- validatedl.sh
+++ validatedl.sh
@@ -4,6 +4,6 @@ mod=$(cat pathmod)
 arma=$(cat patharma)
 dexec='docker exec -t arma'
 
-found=$($dexec ls -1 mod | grep [0-9]* | sed -z 's:\n:|:g' | sed 's:^:":' | sed 's:|$:":')
+found=$($dexec ls $mod | grep -o [0-9]* | sed -z 's:\n:|:g' | sed 's:|$::g')
 grep -Ev "$found" modlist > missingmods
 grep -Ev "$found" missionlist > missingmissions
