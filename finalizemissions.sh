d7f1f143c49041dff50363f5af4654554de50e78
maxhougas <max.hougas@gmail.com>
2024-12-12 16:43:27 -0600

    updated finalizemissions.sh to grab mission name from _legacy.bin file

--- finalizemissions.sh
+++ finalizemissions.sh
@@ -8,10 +8,15 @@ dexec='docker exec -t arma'
 for line in $(cat missionlist)
 do
 
-id=$($dexec echo $line | grep -Po "[0-9]*(?==)")
-mname=$($dexec echo $line | grep -Po "(?<==)[a-zA-Z0-9._-]*")
-file=$($dexec ls -1 $mod/$id | grep -o "[a-zA-Z0-9._-]*")
+id=$(echo $line | grep -Po "[0-9]*(?==)")
+mapname=$(echo $line | grep -Po '(?<=\.)[a-zA-Z0-9_-]*$')
+file=$($dexec ls $mod/$id | grep -o "[a-zA-Z0-9._-]*_legacy.bin")
+missionname=$($dexec grep -Pao '(?<=sourceName.)[ ="]*[a-zA-Z0-9%_-]*' $mod/$id/$file | grep -o '[a-zA-Z0-9%_-]*' | tr [A-Z] [a-z])
 
-$dexec ln -sfT $mod/$id/$file "$arma/mpmissions/$mname.pbo"
+if [ -z $missionname ]; then
+missionname=$(echo $line | grep -Po '(?<==)[a-zA-Z0-9_-]*')
+fi
+
+$dexec ln -sfT $mod/$id/$file "$arma/mpmissions/$missionname.$mapname.pbo"
 
 done
