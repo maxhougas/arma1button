a96669d78a444b4d1169ed52a1227f7c76f2a00e
maxhougas <max.hougas@gmail.com>
2024-12-19 01:02:38 -0600

    fixed error restoring save files to wrong directory

--- saverestore.sh
+++ saverestore.sh
@@ -3,5 +3,6 @@
 if [ -z $1 ]; then
 echo 'need backup time in format YYYYmmdd_HHMMSS'
 else
-docker cp ./Player_$1 arma:"$(cat pathsave)"
+docker cp "./Player_$1/Player.Arma3Profile" arma:"$(cat pathsave)/Player.Arma3Profile"
+docker cp "./Player_$1/Player.vars.Arma3Profile" arma:"$(cat pathsave)/Player.vars.Arma3Profile"
 fi
