06f1f85cd00d813e4a28c1f00ed5c4f3e46cae1b
maxhougas <max.hougas@gmail.com>
2024-12-29 13:39:48 -0600

    trimmed bloated port list

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 docker run -d \
- -p 2344-2345:2344-2345/tcp \
- -p 27015:27015/tcp \
- -p 27036:27036/tcp \
  -p 2302-2306:2302-2306/udp \
- -p 2344:2344/udp \
- -p 27015:27015/udp \
- -p 27031-27036:27031-27036/udp \
  --name arma arma \
  tail -f /dev/null
