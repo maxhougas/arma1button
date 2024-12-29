309763e9009ffcf3e499a8b6ea37b3d6f259b1e6
maxhougas <max.hougas@gmail.com>
2024-12-29 16:00:49 -0600

    added creatordlc to updatearma.sh

--- armaupdate.sh
+++ armaupdate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -t arma steamcmd/steamcmd.sh +login $(cat creds) +app_update 233780 +quit
+docker exec -t arma steamcmd/steamcmd.sh +login $(cat creds) +app_update 233780 -beta creatordlc +quit
