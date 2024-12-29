93ed6b077c11711a924a6e65e225143c9d0ab3d9
maxhougas <max.hougas@gmail.com>
2024-12-29 15:45:46 -0600

    removed file suck operation from build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 #Discharge dockerfile
 echo 'FROM maxhougas/arma3temp:bb' > dockerfile.ar
 echo "RUN su -c 'steamcmd/steamcmd.sh +force_install_dir ~/arma +login $(cat creds) +app_update 233780﻿ -beta creatordlc +quit' user" >> dockerfile.ar
-echo 'COPY --chown=user:user ["config","params","./"]' >> dockerfile.ar
 echo 'RUN echo "arma built from maxhougas/arma3temp:bb on $(date +%Y%m%d)" >> /info.txt' >> dockerfile.ar
 echo 'USER user:user' >> dockerfile.ar
 echo 'CMD tail -f /dev/null' >> dockerfile.ar
