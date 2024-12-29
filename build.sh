61ccfb95249ffa7369498af724ae2d697ed80aa0
maxhougas <max.hougas@gmail.com>
2024-12-29 14:42:49 -0600

    fixed error in steamcmd line

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 #Discharge dockerfile
 echo 'FROM maxhougas/arma3temp:bb' > dockerfile.ar
-echo "RUN su -c 'steamcmd/steamcmd.sh +force_install_dir ~/arma +login $(cat creds) +app_update 233780﻿ +quit user -beta creatordlc +quit' user" >> dockerfile.ar
+echo "RUN su -c 'steamcmd/steamcmd.sh +force_install_dir ~/arma +login $(cat creds) +app_update 233780﻿ -beta creatordlc +quit' user" >> dockerfile.ar
 echo 'COPY --chown=user:user ["config","params","./"]' >> dockerfile.ar
 echo 'RUN echo "arma built from maxhougas/arma3temp:bb on $(date +%Y%m%d)" >> /info.txt' >> dockerfile.ar
 echo 'USER user:user' >> dockerfile.ar
