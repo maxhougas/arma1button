ec38a5ff6851afcb57fdb71c4ec6accde7d87a0b
maxhougas <max.hougas@gmail.com>
2025-08-03 22:58:43 -0500

    file permission issues

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ fi
 #Discharge dockerfile base game layer 
 echo 'FROM maxhougas/arma3:tem' > dockerfile.ar.base
 echo "RUN su -c 'steamcmd/steamcmd.sh +force_install_dir ~/arma +login $(cat creds) +app_update 233780 $base +quit' user" >> dockerfile.ar.base
-echo 'COPY server.cfg ./' >> dockerfile.ar.base
+echo 'COPY --chown=user:user server.cfg ./' >> dockerfile.ar.base
 echo 'RUN echo "arma3:base built from maxhougas/arma3:tem on $(date +%Y%m%d)" >> /info.txt' >> dockerfile.ar.base
 
 #build image
@@ -23,7 +23,7 @@ rm dockerfile.ar.base
 else
 #Discharge dockerfile dlc layer
 echo 'FROM arma3:base' > dockerfile.ar.dlc
-echo 'COPY ["listdlc","listmissions","listmods","./"]' >> dockerfile.ar.dlc
+echo 'COPY --chown=user:user ["listdlc","listmissions","listmods","./"]' >> dockerfile.ar.dlc
 echo "RUN su -c './dlsupdateall.sh && ./dlsreinstall.sh $(cat creds) && ./finalizemissions.sh && ./finalizemods.sh' user" >> dockerfile.ar.dlc
 echo 'RUN echo "arma3:dlc built from arma3:base on $(date +%Y%m%d)" >> /info.txt' >> dockerfile.ar.dlc
 echo 'USER user:user' >> dockerfile.ar.dlc
--- create.sh
+++ create.sh
@@ -3,4 +3,4 @@
 docker create \
  -p 0.0.0.0:2302-2306:2302-2306/udp \
  --name arma arma3:${1:-dlc} \
- sh -c '/home/user/arma/arma3server_x64 -par=/home/user/params'
+ sh -c 'chown -LR user:user /home/user/save /home/user/arma/arma3server_x64 -par=/home/user/params'
